570f8d3c3cbbc89fd28cce5bd4dc39e966b41668
Jon <j.gutierrez@ehu.eus>
2023-09-11 10:48:03 +0000

    custom backbone: modified mmseg/backbones/__init__.py and created mmseg/backbones/internimage.py to use tools/get_flops.py utility

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -7,6 +7,7 @@ from .erfnet import ERFNet
 from .fast_scnn import FastSCNN
 from .hrnet import HRNet
 from .icnet import ICNet
+from .internimage import InternImage
 from .mae import MAE
 from .mit import MixVisionTransformer
 from .mobilenet_v2 import MobileNetV2
@@ -26,5 +27,5 @@ __all__ = [
     'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3',
     'VisionTransformer', 'SwinTransformer', 'MixVisionTransformer',
     'BiSeNetV1', 'BiSeNetV2', 'ICNet', 'TIMMBackbone', 'ERFNet', 'PCPVT',
-    'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE'
+    'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE', 'internimage'
 ]
