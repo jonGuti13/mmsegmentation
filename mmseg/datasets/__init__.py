8f780c8882eb395eb4e52699be1d1dc207ee802d
Jon <j.gutierrez@ehu.eus>
2023-09-20 10:33:31 +0200

    updated mmseg/datasets/__init__.py and created mmseg/datasets/hsi_drive.py to train InternImage with our custom dataset

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -9,6 +9,7 @@ from .dark_zurich import DarkZurichDataset
 from .dataset_wrappers import (ConcatDataset, MultiImageMixDataset,
                                RepeatDataset)
 from .drive import DRIVEDataset
+from .hsi_drive import HSIDrive20
 from .hrf import HRFDataset
 from .isaid import iSAIDDataset
 from .isprs import ISPRSDataset
@@ -24,6 +25,7 @@ __all__ = [
     'DATASETS', 'build_dataset', 'PIPELINES', 'CityscapesDataset',
     'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset',
     'PascalContextDataset59', 'ChaseDB1Dataset', 'DRIVEDataset', 'HRFDataset',
+    'HSIDrive20',
     'STAREDataset', 'DarkZurichDataset', 'NightDrivingDataset',
     'COCOStuffDataset', 'LoveDADataset', 'MultiImageMixDataset',
     'iSAIDDataset', 'ISPRSDataset', 'PotsdamDataset'
